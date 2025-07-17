86dac70d4318b644645d501a0e914b3423255016
Buğra Davut <bugradavut@kaskad.ca>
2025-07-17 16:28:15 +0300

    feat: update .gitignore for better cache and build file handling

--- apps/worker/dist/utils/redis.d.ts
+++
@@ -1,8 +0,0 @@
-import IORedis from "ioredis";
-declare class RedisClient {
-    private static instance;
-    static getInstance(): IORedis;
-    static disconnect(): Promise<void>;
-}
-export { RedisClient };
-//# sourceMappingURL=redis.d.ts.map
